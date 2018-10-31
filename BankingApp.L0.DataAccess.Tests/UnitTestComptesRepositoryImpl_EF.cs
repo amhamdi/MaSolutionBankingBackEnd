2ad823a283d1fe1bafb4a28d42d6bf8fcd2e868b
Mohamed Romdhani <mromdhani7@gmail.com>
2018-10-31 13:54:17 +0100

    Terminaison de la couche DataAccess (avec ses tests)

---
+++ BankingApp.L0.DataAccess.Tests/UnitTestComptesRepositoryImpl_EF.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using BankingApp.L0.DataAccess.Domain;
+using BankingApp.L0.DataAccess.Implementations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankingApp.L0.DataAccess.Tests
+{
+    [TestClass]
+    public class UnitTestComptesRepositoryImpl_EF
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            // ARRANGE
+            var nouveau = new Compte
+              { Numero="C4000", Proprietaire = "Seifeddine", Solde=4000M};
+            var underTest = new ComptesRepositoryImpl_EF();
+
+            // ACT
+            underTest.Create(nouveau);
+
+            // ASSERT
+        }
+        [TestMethod]
+        public void TestFindById_Nominal()
+        {
+            // ARRANGE
+            var underTest = new ComptesRepositoryImpl_EF();
+            var numero = "C4000";
+            var expected = new Compte { Numero = "C4000", Proprietaire = "Seifeddine", Solde = 4000M };
+           
+
+            // ACT
+            var actual = underTest.FindById(numero);
+
+            // ASSERT
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestFindById_Degradé()
+        {
+            // ARRANGE
+            var underTest = new ComptesRepositoryImpl_EF();
+            var numero = "NNNNNNN";        
+
+
+            // ACT
+            var actual = underTest.FindById(numero);
+
+            // ASSERT
+            Assert.IsNull(actual);
+        }
+        [TestMethod]
+        public void TestFindAll()
+        {
+            // ARRANGE
+            var underTest = new ComptesRepositoryImpl_EF();
+            
+            // ACT
+            var actual =  (List<Compte>)underTest.FindAll();
+
+            // ASSERT
+            Assert.AreEqual(actual.Count, 4 );
+        }
+    }
+}
---
+++ BankingApp.L0.DataAccess/Facade/IComptesRepository.cs
@@ -0,0 +1,24 @@
+﻿using BankingApp.L0.DataAccess.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.L0.DataAccess.Facade
+{
+    public interface IComptesRepository
+    {
+        // CRUD Contract
+
+        void Create(Compte c);  // C du CRUD
+        Compte FindById(string id); //  R (Retrieve) = Find = Get
+
+        IEnumerable<Compte> FindAll();    // un autre R
+
+        void Update(Compte c);    // U du CRUD
+
+        void Delete(string id); // D du CRUD
+
+    }
+}
